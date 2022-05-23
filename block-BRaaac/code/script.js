571f4456ff088b8cec27d3fc812d9a7e76cce812
Abhishek Agarwal <abhishekagarwal151@gmail.com>
2022-05-23 09:38:49 +0530

    final-push

--- block-BRaaac/code/script.js
+++ block-BRaaac/code/script.js
@@ -29,19 +29,50 @@ function handleChange(event){
     allMovies[id].watched=!allMovies[id].watched;
     createMovieUI(allMovies,rootElm);
 }
+
+
+function createElement(type,attr={},...children){
+    let element=document.createElement(type);
+    for(let key in attr){
+        if(key.startsWith('data-')){
+            element.setAttribute(key,attr[key]);
+        }else if(key.startsWith('on')){
+            let eventType=key.replace('on','')
+            element.addEventListener(eventType,attr[key])
+        }
+        
+        else{
+            element[key]=attr[key];
+        }
+    }
+    children.forEach((child)=>{
+        if(typeof child === 'object'){
+            element.append(child);
+        }
+        if(typeof child === 'string'){
+            let node=document.createTextNode(child);
+            element.append(node);
+        }
+    });
+    return element;
+
+}
 function createMovieUI(data,root){
     root.innerHTML='';
     data.forEach((movie,i)=>{
-        let li=document.createElement('li');
-        let button=document.createElement('button');;
-        button.id=i;
-        button.innerText=movie.watched? 'Watched': 'To Watch';
-        button.addEventListener('click',handleChange);
-        let label=document.createElement('label');
-        label.for=i;
-        label.innerText=movie.name;
-
-        li.append(label,button);
+        let li=createElement('li',
+         null, 
+         createElement('label',{for:i},movie.name),
+         createElement('button', {id:i, onclick:handleChange},
+         movie.watched ? 'Watched':'To Watch')
+         );
+        // let button=document.createElement('button');;
+        // button.id=i;
+        // button.innerText=movie.watched? 'Watched': 'To Watch';
+        // button.addEventListener('click',handleChange);
+        // let label=document.createElement('label');
+        // label.for=i;
+        // label.innerText=movie.name;
 
         rootElm.append(li);
     });
